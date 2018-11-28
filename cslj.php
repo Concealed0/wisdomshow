77dbbc587cd98a9f6176f8217db985a576a82fe1
AnGe0 <384299160@qq.com>
2018-11-28 09:17:41 +0800

    Update cslj.php

--- cslj.php
+++ cslj.php
@@ -1,6 +1,6 @@
 <?php
 header("content-type=text/html;charset=utf-8");
-$mysqli = new mysqli('210.44.53.190:3306','root','mysql@123','wisdom');
+$mysqli = new mysqli('***','root','****','wisdom');
 if($mysqli -> connect_errno){
 	die("数据库连接失败".$mysqli -> connect_error);
 }
